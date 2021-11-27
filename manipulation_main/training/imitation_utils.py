e1a870c1da8721c07ce114ebe908e6b69291e6c5
Patrick Kao <patrick.d.kao@gmail.com>
2021-11-27 00:17:39 -0500

    added bc script

---
+++ manipulation_main/training/imitation_utils.py
@@ -0,0 +1,52 @@
+import logging
+import os
+
+import gym
+import stable_baselines as sb
+from gym.wrappers import Monitor
+from stable_baselines.common.vec_env import DummyVecEnv
+
+from manipulation_main.common import io_utils
+from manipulation_main.training.wrapper import TimeFeatureWrapper
+
+_script_dir = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.join(_script_dir, "..", "..")
+
+
+def get_env_expert(args):
+    config_dir = os.path.join(BASE_DIR, args.config)
+    config = io_utils.load_yaml(config_dir)
+    # os.mkdir(args.model_dir)
+    # Folder for best models
+    # os.mkdir(args.model_dir + "/best_model")
+    model_dir = args.model_dir
+    algo = args.algo
+
+    if args.visualize:
+        config['simulation']['real_time'] = False
+        config['simulation']['visualize'] = True
+    if args.simple:
+        logging.info("Simplified environment is set")
+        config['simplified'] = True
+    if args.shaped:
+        logging.info("Shaped reward function is being used")
+        config['reward']['shaped'] = True
+    if args.timestep:
+        config[algo]['total_timesteps'] = args.timestep
+    if not args.algo == 'DQN':
+        config['robot']['discrete'] = False
+    else:
+        config['robot']['discrete'] = True
+
+    config[algo]['save_dir'] = model_dir
+    if args.timefeature:
+        env = DummyVecEnv([lambda: TimeFeatureWrapper(gym.make('gripper-env-v0', config=config))])
+    else:
+        # force = true to overwrite
+        env = DummyVecEnv(
+            [lambda: Monitor(gym.make('gripper-env-v0', config=config),
+                             os.path.join(model_dir, "dagger", "gym_log_file"),
+                             force=True)])
+
+    agent = sb.SAC.load(os.path.join(BASE_DIR, args.model))
+    return env, agent
--- manipulation_main/training/run_bc.py
+++ manipulation_main/training/run_bc.py
@@ -2,17 +2,28 @@ import argparse
 import os.path
 
 from imitation.algorithms.bc import BC
-from imitation.algorithms.dagger import SimpleDAggerTrainer
+from imitation.data import rollout
+from imitation.scripts.common.demonstrations import load_expert_trajs
 from imitation.util import logger
 from stable_baselines3.common.policies import ActorCriticCnnPolicy
 
 from manipulation_main.training.custom_obs_policy import TransposeNatureCNN
 from manipulation_main.training.imitation_utils import BASE_DIR, get_env_expert
 
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def rollout_expert_traj(save_path: str, expert, env, min_timesteps: int, min_episodes: int):
+    sample_until = rollout.make_sample_until(
+        min_timesteps,
+        min_episodes
+    )
+    rollout.rollout_and_save(save_path, expert, env, sample_until)
+
 
 def main(args):
     os.chdir(BASE_DIR)  # all filepaths are relative to repo root in other files
-    dagger_dir = os.path.join(BASE_DIR, args.model_dir, "dagger")
+    bc_dir = os.path.join(BASE_DIR, args.model_dir, "bc")
 
     env, expert = get_env_expert(args)
 
@@ -23,17 +34,25 @@ def main(args):
     # TODO: is lr_schedule used? For now using constant lr scheduler
     train_policy = ActorCriticCnnPolicy(observation_space=ob_space, action_space=ac_space, lr_schedule=lambda x: 0.005,
                                         features_extractor_class=TransposeNatureCNN)
-    log_dir = os.path.join(dagger_dir, "logs")
-    bc_trainer = BC(observation_space=ob_space, action_space=ac_space, demonstrations=None, policy=train_policy)
 
-    # construct dagger instance and train
-    dagger_logger = logger.configure(log_dir)
-    save_dir = os.path.join(dagger_dir, "dagger_model")
-    trainer = SimpleDAggerTrainer(venv=env, scratch_dir=save_dir, expert_policy=expert, bc_trainer=bc_trainer,
-                                  custom_logger=dagger_logger)
-    trainer.train(total_timesteps=args.num_timesteps, rollout_round_min_timesteps=100)
+    # load/create rollouts
+    rollout_file = os.path.join(BASE_DIR, args.rollout_file)
+    if not os.path.exists(rollout_file):
+        # do rollouts
+        rollout_expert_traj(rollout_file, expert, env, min_timesteps=args.num_timesteps,
+                            min_episodes=args.num_episodes)
 
-    trainer.save_trainer()
+    # load rollouts
+    demos = load_expert_trajs(rollout_file)
+
+    log_dir = os.path.join(bc_dir, "logs")
+    bc_logger = logger.configure(log_dir)
+    bc_trainer = BC(observation_space=ob_space, action_space=ac_space, demonstrations=demos, policy=train_policy,
+                    custom_logger=bc_logger)
+
+    # train bc
+    bc_trainer.train()
+    bc_trainer.save_policy(policy_path=os.path.join(bc_dir, "final_policy.pt"))
 
 
 if __name__ == "__main__":
@@ -43,6 +62,8 @@ if __name__ == "__main__":
     train_parser.add_argument('--algo', type=str, required=True)
     train_parser.add_argument('--model', type=str)
     train_parser.add_argument("--num_timesteps", type=int)
+    train_parser.add_argument("--num_episodes", type=int)
+    train_parser.add_argument("--rollout_file", type=str, required=True)
 
     train_parser.add_argument('--timestep', type=str)
     train_parser.add_argument('-s', '--simple', action='store_true')
